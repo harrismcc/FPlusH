96c124294e9d2165c1dbc05b8c4da7cfaf1c0cea
harris mccullers <harrismcc@gmail.com>
2016-12-15 14:54:15 -0800

    Initial Commit

---
+++ QuestionApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuestionApp
+//
+//  Created by Harris McCullers on 12/15/16.
+//  Copyright © 2016 Harris+Fran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
