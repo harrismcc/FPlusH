c0e0d4dc694df5457bf06b718a56ee1d4119e2e5
harrismcc <harrismcc@gmail.com>
2017-01-06 14:43:15 -0800

    Fixed a stupid pesky error

--- QuestionApp/ViewController.swift
+++ QuestionApp/ViewController.swift
@@ -14,9 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        if ([FBSDKAccessToken, currentAccessToken]) {
-            // User is logged in, do work such as go to next view controller.
-        }
+
     }
 
     override func didReceiveMemoryWarning() {
